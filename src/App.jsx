16152467dd4cd5176572059da55821cc8718fc6d
NathanDSanta <natan.d.santa@gmail.com>
2026-01-05 20:30:56 +0100

    made the video with the custom shape

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import About from "./components/About/About";
-import Attractions from "./components/Attractions/Attractions";
 import Activities from "./components/Activities/Activities";
-import Cart from "./components/Cart/Cart";
 import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
-import Footer from "./components/Footer/Footer";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Hero />
-      <About />
-      <Attractions />
       <Activities />
-      <Cart />
       <AudioPlayer />
-      <Footer />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Hero.css";
 
+
 function Hero() {
   return (
     <div className="Hero">
-      <h1>Hero</h1>
+      <video autoPlay loop muted>
+        <source src="/assets/video/banyoles.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
